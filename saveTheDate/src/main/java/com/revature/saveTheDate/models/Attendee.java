f70e60f423e29babef1301a4a274eb33046528de
Austin Ristau <austinristausc@gmail.com>
2022-02-21 11:15:06 -0600

    attendee model with all functionality

--- saveTheDate/src/main/java/com/revature/saveTheDate/models/Attendee.java
+++ saveTheDate/src/main/java/com/revature/saveTheDate/models/Attendee.java
@@ -1,5 +1,7 @@
 package com.revature.saveTheDate.models;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -30,4 +32,100 @@ public class Attendee {
 	@Column(name="pluse_one_dinner_id")
 	private int plusOneDinnerId;
 
+	public Attendee() {
+		super();
+	}
+
+	public Attendee(int id, int userId, int weddingId, boolean isAttending, int dinnerId, boolean plusOne,
+			int plusOneDinnerId) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.weddingId = weddingId;
+		this.isAttending = isAttending;
+		this.dinnerId = dinnerId;
+		this.plusOne = plusOne;
+		this.plusOneDinnerId = plusOneDinnerId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getWeddingId() {
+		return weddingId;
+	}
+
+	public void setWeddingId(int weddingId) {
+		this.weddingId = weddingId;
+	}
+
+	public boolean isAttending() {
+		return isAttending;
+	}
+
+	public void setAttending(boolean isAttending) {
+		this.isAttending = isAttending;
+	}
+
+	public int getDinnerId() {
+		return dinnerId;
+	}
+
+	public void setDinnerId(int dinnerId) {
+		this.dinnerId = dinnerId;
+	}
+
+	public boolean isPlusOne() {
+		return plusOne;
+	}
+
+	public void setPlusOne(boolean plusOne) {
+		this.plusOne = plusOne;
+	}
+
+	public int getPlusOneDinnerId() {
+		return plusOneDinnerId;
+	}
+
+	public void setPlusOneDinnerId(int plusOneDinnerId) {
+		this.plusOneDinnerId = plusOneDinnerId;
+	}
+
+	@Override
+	public String toString() {
+		return "Attendee [id=" + id + ", userId=" + userId + ", weddingId=" + weddingId + ", isAttending=" + isAttending
+				+ ", dinnerId=" + dinnerId + ", plusOne=" + plusOne + ", plusOneDinnerId=" + plusOneDinnerId + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dinnerId, id, isAttending, plusOne, plusOneDinnerId, userId, weddingId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Attendee other = (Attendee) obj;
+		return dinnerId == other.dinnerId && id == other.id && isAttending == other.isAttending
+				&& plusOne == other.plusOne && plusOneDinnerId == other.plusOneDinnerId && userId == other.userId
+				&& weddingId == other.weddingId;
+	}
 }
