4a9f1a09e494f53f24945e75c9f7904b497c1a33
Michael Bronzo <82384863+BronzoMichael@users.noreply.github.com>
2022-02-22 15:50:32 -0500

    Update WeddingServices.java

--- saveTheDate/src/main/java/com/revature/saveTheDate/services/WeddingServices.java
+++ saveTheDate/src/main/java/com/revature/saveTheDate/services/WeddingServices.java
@@ -1,5 +1,45 @@
 package com.revature.saveTheDate.services;
 
+import java.util.List;
+
+import com.revature.saveTheDate.daos.WeddingDAO;
+import com.revature.saveTheDate.models.Wedding;
+
 public class WeddingServices {
+
+
+	private final WeddingDAO weddingDAO;
+
+	public WeddingServices(WeddingDAO weddingDAO) {
+		this.weddingDAO = weddingDAO;
+	}
+
+	public boolean addWedding(Wedding wedding) {
+
+		return weddingDAO.addWedding(wedding);
+	}
+
+	public List<Wedding> getAllWeddings() {
+		return weddingDAO.getAllWeddings();
+
+	}
+
+	public Wedding getWeddingById(int id) {
+
+		return weddingDAO.getWeddingById(id);
+	}
+
+	public void updateWeddingWithSessionMethod(Wedding wedding) {
+
+		weddingDAO.updateWeddingWithSessionMethod(wedding);
+	}
+		
+	public void updateWeddingWithHQL(Wedding wedding) {
+		weddingDAO.updatedWeddingWithHQL(wedding);
+	}
+	
+	public void deleteWedding (int id) {
+		weddingDAO.deleteWedding(id);
+	}
 
 }
