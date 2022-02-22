9d685f5c83a29eb05aec705df954b401ec54cf73
Michael Bronzo <82384863+BronzoMichael@users.noreply.github.com>
2022-02-22 15:48:04 -0500

    Update AttendeeServices.java

--- saveTheDate/src/main/java/com/revature/saveTheDate/services/AttendeeServices.java
+++ saveTheDate/src/main/java/com/revature/saveTheDate/services/AttendeeServices.java
@@ -1,5 +1,41 @@
-package com.revature.saveTheDate.services;
+Package com.revature.saveTheDate.services;
 
-public class AttendeeServices {
+import java.util.List;
 
+import com.revature.saveTheDate.daos.AttendeeDAO;
+import com.revature.saveTheDate.models.Attendee;
+
+public class AtendeeServices {
+
+	private final AttendeeDAO attendeeDAO;
+
+	public AttendeeServices(AttendeeDAO attendeeDAO) {
+		this.attendeeDAO = attendeeDAO;
+	}
+
+	public boolean addAttendee(Attendee attendee) {
+
+		return attendeeDAO.addAttendee(attendee);
+	}
+
+	public List<Attendee> getAllAttendees() {
+		return attendeeDAO.getAllAttendee();
+
+	}
+
+	public Attendee getAttendeeById(int id) {
+
+		return attendeeDAO.getAttendeeById(id);
+	}
+
+	public void updateAttendeeWithSessionMethod(Attendee attendee) {
+
+		attendeeDAO.updateAttendeeWithSessionMethod(attendee);
+	}
+	
+	public void deleteAttendee (int id) {
+		attendeeDAO.deleteAttendee(id);
+	}
+
+	
 }
