1f3e501b516eb7d60199f3c15061012918f8e991
Michael Bronzo <82384863+BronzoMichael@users.noreply.github.com>
2022-02-22 15:49:20 -0500

    Update ServiceServices.java

--- saveTheDate/src/main/java/com/revature/saveTheDate/services/ServiceServices.java
+++ saveTheDate/src/main/java/com/revature/saveTheDate/services/ServiceServices.java
@@ -1,5 +1,41 @@
 package com.revature.saveTheDate.services;
 
+import java.util.List;
+
+import com.revature.saveTheDate.daos.ServiceDAO;
+import com.revature.saveTheDate.models.Service;
+
 public class ServiceServices {
+
+
+	private final ServiceDAO serviceDAO;
+
+	public ServiceServices(ServiceDAO serviceDAO) {
+		this.serviceDAO = serviceDAO;
+	}
+
+	public boolean addService(Service service) {
+
+		return serviceDAO.addService(service);
+	}
+
+	public List<Service> getAllService() {
+		return serviceDAO.getAllService();
+
+	}
+
+	public Service getServiceById(int id) {
+
+		return serviceDAO.getServiceById(id);
+	}
+
+	public void updateServiceWithSessionMethod(Service service) {
+
+		serviceDAO.updateServiceWithSessionMethod(service);
+	}
+		
+	public void deleteService (int id) {
+		serviceDAO.deleteService(id);
+	}
 
 }
