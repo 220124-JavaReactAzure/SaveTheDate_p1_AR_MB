7a51491d16c2e09f612e9cdc83d2578f46ab7cd8
Michael Bronzo <82384863+BronzoMichael@users.noreply.github.com>
2022-02-22 15:48:54 -0500

    Update RoleServices.java

--- saveTheDate/src/main/java/com/revature/saveTheDate/services/RoleServices.java
+++ saveTheDate/src/main/java/com/revature/saveTheDate/services/RoleServices.java
@@ -1,5 +1,41 @@
 package com.revature.saveTheDate.services;
 
+import java.util.List;
+
+import com.revature.saveTheDate.daos.RoleDAO;
+import com.revature.saveTheDate.models.Role;
+
 public class RoleServices {
+
+
+	private final RoleDAO roleDAO;
+
+	public RoleServices(RoleDAO roleDAO) {
+		this.roleDAO = roleDAO;
+	}
+
+	public boolean addRole(Role role) {
+
+		return roleDAO.addRole(role);
+	}
+
+	public List<Role> getAllRole() {
+		return roleDAO.getAllRole();
+
+	}
+
+	public Role getRoleById(int id) {
+
+		return roleDAO.getRoleById(id);
+	}
+
+	public void updateRoleWithSessionMethod(Role role) {
+
+		roleDAO.updateRoleWithSessionMethod(role);
+	}
+		
+	public void deleteRole (int id) {
+		roleDAO.deleteRole(id);
+	}
 
 }
