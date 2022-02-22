bd2a556da57713cd17f2ca2d7ec6af2931ab304d
Michael Bronzo <82384863+BronzoMichael@users.noreply.github.com>
2022-02-22 15:48:31 -0500

    Update DinnerServices.java

--- saveTheDate/src/main/java/com/revature/saveTheDate/services/DinnerServices.java
+++ saveTheDate/src/main/java/com/revature/saveTheDate/services/DinnerServices.java
@@ -1,5 +1,41 @@
 package com.revature.saveTheDate.services;
 
+import java.util.List;
+
+import com.revature.saveTheDate.daos.DinnerDAO;
+import com.revature.saveTheDate.models.Dinner;
+
 public class DinnerServices {
 
+	private final DinnerDAO dinnerDAO;
+
+	public DinnerServices(DinnerDAO dinnerDAO) {
+		this.dinnerDAO = dinnerDAO;
+	}
+
+	public boolean addDinner(Dinner dinner) {
+
+		return dinnerDAO.addDinner(dinner);
+	}
+
+	public List<Dinner> getAllDinners() {
+		return dinnerDAO.getAllDinners();
+
+	}
+
+	public Dinner getDinnerById(int id) {
+
+		return dinnerDAO.getDinnerById(id);
+	}
+
+	public void updateDinnerWithSessionMethod(Dinner dinner) {
+
+		dinnerDAO.updateDinnerWithSessionMethod(dinner);
+	}
+	
+	public void deleteDinner (int id) {
+		dinnerDAO.deleteDinner(id);
+	}
+
+	
 }
