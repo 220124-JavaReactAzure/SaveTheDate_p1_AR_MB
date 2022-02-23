47220da5c5941b4a3d721c41200dbb60ddb387ee
Michael Bronzo <bronzomichael@gmail.com>
2022-02-23 18:01:03 -0500

    business logic

--- saveTheDate/src/main/java/com/revature/saveTheDate/services/WeddingServices.java
+++ saveTheDate/src/main/java/com/revature/saveTheDate/services/WeddingServices.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.revature.saveTheDate.daos.WeddingDAO;
 import com.revature.saveTheDate.models.Wedding;
+import com.revature.saveTheDate.daos.ServiceDAO;
+import com.revature.saveTheDate.services.ServiceServices;
 
 public class WeddingServices {
 
@@ -15,8 +17,14 @@ public class WeddingServices {
 	}
 
 	public boolean addWedding(Wedding wedding) {
-
-		return weddingDAO.addWedding(wedding);
+		// Check if wedding under budget then check if all services available on that date 
+		if (isWeddingUnderBudget(wedding) && isWeddingVenueAvailable(wedding) && isWeddingCatererAvailable(wedding) && isWeddingFloristAvailable(wedding) &&
+		isWeddingPhotographerAvailable(wedding) && isWeddingPhotographerAvailable(wedding)){
+		weddingDAO.addWedding(wedding);
+		return true;
+		} else{
+			return false;
+		}
 	}
 
 	public List<Wedding> getAllWeddings() {
@@ -37,6 +45,71 @@ public class WeddingServices {
 	
 	public void deleteWeddingById (int id) {
 		weddingDAO.deleteWeddingById(id);
+	}
+
+	public boolean isWeddingUnderBudget(Wedding wedding){
+		if (ServiceServices.getServiceById(wedding.getVenueId()).getCost() + ServiceServices.getServiceById(wedding.getCatererId()).getCost() +
+		ServiceServices.getServiceById(wedding.getFloristId()).getCost() + ServiceServices.getServiceById(wedding.getPhotographerId)().getCost() +
+		ServiceServices.getServiceById(wedding.getMusicianId()).getCost() > wedding.getBudget()){
+			return false;
+		} else {
+		return true;
+		}
+	}
+
+	public boolean isWeddingVenueAvailable (Wedding wedding){
+		for (Wedding weddingCheck : getAllWeddings()){
+			if (weddingCheck.getDate() == wedding.getDate()){
+				if (weddingCheck.getVenueId() == wedding.getVenueId()){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public boolean isWeddingCatererAvailable (Wedding wedding){
+		for (Wedding weddingCheck : getAllWeddings()){
+			if (weddingCheck.getDate() == wedding.getDate()){
+				if (weddingCheck.getCatererId() == wedding.getCatererId()){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public boolean isWeddingFloristAvailable (Wedding wedding){
+		for (Wedding weddingCheck : getAllWeddings()){
+			if (weddingCheck.getDate() == wedding.getDate()){
+				if (weddingCheck.getFloristId() == wedding.getFloristId()){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public boolean isWeddingPhotographerAvailable (Wedding wedding){
+		for (Wedding weddingCheck : getAllWeddings()){
+			if (weddingCheck.getDate() == wedding.getDate()){
+				if (weddingCheck.getPhotographerId() == wedding.getPhotographerId()){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public boolean isWeddingMusicianAvailable (Wedding wedding){
+		for (Wedding weddingCheck : getAllWeddings()){
+			if (weddingCheck.getDate() == wedding.getDate()){
+				if (weddingCheck.getMusicianId() == wedding.getMusicianId()){
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 }
