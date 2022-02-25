f9b48470ccef7e259f5f1ba29fa4ab15ce9972e1
Michael Bronzo <82384863+BronzoMichael@users.noreply.github.com>
2022-02-24 21:13:24 -0500

    Update DinnerServices.java

--- saveTheDate/src/main/java/com/revature/saveTheDate/services/DinnerServices.java
+++ saveTheDate/src/main/java/com/revature/saveTheDate/services/DinnerServices.java
@@ -7,35 +7,35 @@ import com.revature.saveTheDate.models.Dinner;
 
 public class DinnerServices {
 private final DinnerDAO dinnerDAO;
-	private final Logger logger = LogManager.getLogger();
+	//private final Logger logger = LogManager.getLogger();
 
 	public DinnerServices(DinnerDAO dinnerDAO) {
 		this.dinnerDAO = dinnerDAO;
-		logger.info("DinnerServices created");
+		//logger.info("DinnerServices created");
 	}
 
 	public boolean addDinner(Dinner dinner) {
-		logger.info("DinnerServices.addDinner was called for Dinner: " + dinner);
+		//logger.info("DinnerServices.addDinner was called for Dinner: " + dinner);
 		return dinnerDAO.addDinner(dinner);
 	}
 
 	public List<Dinner> getAllDinners() {
-		logger.info("DinnerServices.getAllDinners called");
+		//logger.info("DinnerServices.getAllDinners called");
 		return dinnerDAO.getAllDinners();
 	}
 
 	public Dinner getDinnerById(int id) {
-		logger.info("DinnerServices.getDinnerById was called for id: " + id);
+		//logger.info("DinnerServices.getDinnerById was called for id: " + id);
 		return dinnerDAO.getDinnerById(id);
 	}
 
 	public void updateDinnerWithSessionMethod(Dinner dinner) {
-		logger.info("DinnerServices.updateDinnerWithSessionMethod was called for Dinner: " + dinner);
+		//logger.info("DinnerServices.updateDinnerWithSessionMethod was called for Dinner: " + dinner);
 		dinnerDAO.updateDinnerWithSessionMethod(dinner);
 	}
 	
 	public void deleteDinnerById (int id) {
-		logger.info("DinnerServices.deleteDinnerById was called for id: " + id);
+		//logger.info("DinnerServices.deleteDinnerById was called for id: " + id);
 		dinnerDAO.deleteDinnerById(id);
 	}
 
