b8ba481c12ed45240330bdbfa068000c89dc6529
Michael Bronzo <82384863+BronzoMichael@users.noreply.github.com>
2022-02-24 22:43:49 -0500

    Update AttendeeServices.java

--- saveTheDate/src/main/java/com/revature/saveTheDate/services/AttendeeServices.java
+++ saveTheDate/src/main/java/com/revature/saveTheDate/services/AttendeeServices.java
@@ -8,36 +8,47 @@ import com.revature.saveTheDate.models.Attendee;
 public class AttendeeServices {
 
 	private final AttendeeDAO attendeeDAO;
-	//private final Logger logger = LogManager.getLogger();
+	private final Logger logger = LogManager.getLogger();
 
 	public AttendeeServices(AttendeeDAO attendeeDAO) {
 		this.attendeeDAO = attendeeDAO;
-		//logger.info("AttendeeServices created");
+		logger.info("AttendeeServices created");
 	}
 
 	public boolean addAttendee(Attendee attendee) {
-		//logger.info("AttendeeServices.addAttendee called for Attendee: " + attendee);
+		logger.info("AttendeeServices.addAttendee called for Attendee: " + attendee);
 		return attendeeDAO.addAttendee(attendee);
 	}
 
 	public List<Attendee> getAllAttendees() {
-		//logger.info("AttendeeServices.getAllAttendees called");
+		logger.info("AttendeeServices.getAllAttendees called");
 		return attendeeDAO.getAllAttendee();
 	}
 
 	public Attendee getAttendeeById(int id) {
-		//logger.info("AttendeeServices.getAttendeeById called for id: " + id);
+		logger.info("AttendeeServices.getAttendeeById called for id: " + id);
 		return attendeeDAO.getAttendeeById(id);
 	}
 
 	public void updateAttendeeWithSessionMethod(Attendee attendee) {
-		//logger.info("AttendeeServices.updateAttendeeWithSessionMethod called for Attendee: " + attendee);
+		logger.info("AttendeeServices.updateAttendeeWithSessionMethod called for Attendee: " + attendee);
 		attendeeDAO.updateAttendeeWithSessionMethod(attendee);
 	}
 	
 	public void deleteAttendeeById (int id) {
-		//logger.info("AttendeeServices.deleteAttendeeById called for id: " + id);
+		logger.info("AttendeeServices.deleteAttendeeById called for id: " + id);
 		attendeeDAO.deleteAttendeeById(id);
+	}
+	
+	public boolean isAttendeeValid(Attendee attendee) {
+		if(attendee.isAttending()) {
+			if (attendee.getDinner().getDinnername() == null){
+				return false;
+			} else {
+				return true;
+			}
+		}
+		return true;
 	}
 	
 }
