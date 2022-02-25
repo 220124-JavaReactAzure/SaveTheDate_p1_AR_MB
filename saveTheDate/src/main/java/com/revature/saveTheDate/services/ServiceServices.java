964f704bdbfb32acb60860a84893f7fce5fcd0e0
Michael Bronzo <82384863+BronzoMichael@users.noreply.github.com>
2022-02-24 20:13:45 -0500

    Update ServiceServices.java

--- saveTheDate/src/main/java/com/revature/saveTheDate/services/ServiceServices.java
+++ saveTheDate/src/main/java/com/revature/saveTheDate/services/ServiceServices.java
@@ -7,35 +7,37 @@ import com.revature.saveTheDate.models.Service;
 
 public class ServiceServices {
 
-
-	private final ServiceDAO serviceDAO;
+private final ServiceDAO serviceDAO;
+	private final Logger logger = LogManager.getLogger();
 
 	public ServiceServices(ServiceDAO serviceDAO) {
 		this.serviceDAO = serviceDAO;
+		logger.info("ServiceServices created");
 	}
 
 	public boolean addService(Service service) {
-
+		logger.info("ServiceServices.addService called for Service: " + service);
 		return serviceDAO.addService(service);
 	}
 
 	public List<Service> getAllServices() {
+		logger.info("ServiceServices.getAllServices called");
 		return serviceDAO.getAllService();
-
 	}
 
 	public Service getServiceById(int id) {
-
+		logger.info("ServiceServices.getServiceById called for id: " + id);
 		return serviceDAO.getServiceById(id);
 	}
 
 	public void updateServiceWithSessionMethod(Service service) {
-
+		logger.info("ServiceServices.updateServiceWithSessionMethod called for Service: " + service);
 		serviceDAO.updateServiceWithSessionMethod(service);
 	}
 		
-	public void deleteServiceById (int id) {
-		serviceDAO.deleteServiceById(id);
+	public void deleteService (int id) {
+		logger.info("ServiceServices.deleteService called for Service: " + id);
+		serviceDAO.deleteService(id);
 	}
 
 }
