c38aee41a4d363ebf50a07d9550e6ad04a5d618c
Michael Bronzo <82384863+BronzoMichael@users.noreply.github.com>
2022-02-24 21:13:07 -0500

    Update RoleServices.java

--- saveTheDate/src/main/java/com/revature/saveTheDate/services/RoleServices.java
+++ saveTheDate/src/main/java/com/revature/saveTheDate/services/RoleServices.java
@@ -8,35 +8,35 @@ import com.revature.saveTheDate.models.Role;
 public class RoleServices {
 
 private final RoleDAO roleDAO;
-	private final Logger logger = LogManager.getLogger();
+	//private final Logger logger = LogManager.getLogger();
 
 	public RoleServices(RoleDAO roleDAO) {
 		this.roleDAO = roleDAO;
-		logger.info("RoleServices created");
+		//logger.info("RoleServices created");
 	}
 
 	public boolean addRole(Role role) {
-		logger.info("RoleServices.addRole called for Role: " + role);
+		//logger.info("RoleServices.addRole called for Role: " + role);
 		return roleDAO.addRole(role);
 	}
 
 	public List<Role> getAllRoles() {
-		logger.info("RoleServices.getAllRoles called");
+		//logger.info("RoleServices.getAllRoles called");
 		return roleDAO.getAllRole();
 	}
 
 	public Role getRoleById(int id) {
-		logger.info("RoleServices.getRoleById called for id: " + id);
+		//logger.info("RoleServices.getRoleById called for id: " + id);
 		return roleDAO.getRoleById(id);
 	}
 
 	public void updateRoleWithSessionMethod(Role role) {
-		logger.info("RoleServices.updateRoleWithSessionMethod called for Role: " + role);
+		//logger.info("RoleServices.updateRoleWithSessionMethod called for Role: " + role);
 		roleDAO.updateRoleWithSessionMethod(role);
 	}
 		
 	public void deleteRoleById (int id) {
-		logger.info("RoleServices.deleteRoleById called for id: " + id);
+		//logger.info("RoleServices.deleteRoleById called for id: " + id);
 		roleDAO.deleteRoleById(id);
 	}
 
