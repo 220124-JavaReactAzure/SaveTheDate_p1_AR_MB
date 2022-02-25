da883227a64db1d77a698aaabad5aa3497397c2c
Michael Bronzo <82384863+BronzoMichael@users.noreply.github.com>
2022-02-24 21:10:50 -0500

    Update WeddingServices.java

--- saveTheDate/src/main/java/com/revature/saveTheDate/services/WeddingServices.java
+++ saveTheDate/src/main/java/com/revature/saveTheDate/services/WeddingServices.java
@@ -13,15 +13,15 @@ public class WeddingServices {
 
 	private final WeddingDAO weddingDAO;
 	private ServiceServices serviceServices;
-	private final Logger logger = LogManager.getLogger();
+	//private final Logger logger = LogManager.getLogger();
 
 	public WeddingServices(WeddingDAO weddingDAO) {
 		this.weddingDAO = weddingDAO;
-		logger.info("Weddding Services created");
+		//logger.info("Weddding Services created");
 	}
 
 	public boolean addWedding(Wedding wedding) {
-		logger.info("WeddingService.addWedding was called for wedding: " + wedding);
+		//logger.info("WeddingService.addWedding was called for wedding: " + wedding);
 		// Check if wedding under budget then check if all services available on that date 
 		if (isWeddingValid(wedding) && isWeddingUnderBudget(wedding) && isWeddingVenueAvailable(wedding) && isWeddingCatererAvailable(wedding) && isWeddingFloristAvailable(wedding) &&
 		isWeddingPhotographerAvailable(wedding) && isWeddingPhotographerAvailable(wedding) && isWeddingVenueAVenue(wedding) && isWeddingCatererACaterer(wedding) && isWeddingFloristAFlorist(wedding) 
@@ -34,27 +34,27 @@ public class WeddingServices {
 	}
 
 	public List<Wedding> getAllWeddings() {
-		logger.info("Called WeddingService.getAllWeddings to return a list of all existing weddings");
+		//logger.info("Called WeddingService.getAllWeddings to return a list of all existing weddings");
 		return weddingDAO.getAllWeddings();
 	}
 
 	public Wedding getWeddingById(int id) {
-		logger.info("WeddingService.getWeddingById was called using the id: " + id);
+		//logger.info("WeddingService.getWeddingById was called using the id: " + id);
 		return weddingDAO.getWeddingById(id);
 	}
 
 	public void updateWeddingWithSessionMethod(Wedding wedding) {
 		weddingDAO.updateWeddingWithSessionMethod(wedding);
-		logger.info("WeddingService.updateWeddingWithSessionMethod was called for the wedding: " + wedding);
+		//logger.info("WeddingService.updateWeddingWithSessionMethod was called for the wedding: " + wedding);
 	}
 		
 	public void deleteWeddingById (int id) {
 		weddingDAO.deleteWeddingById(id);
-		logger.info("WeddingService.deleteWeddingById was called for the id: " + id);
+		//logger.info("WeddingService.deleteWeddingById was called for the id: " + id);
 	}
 
 	public boolean isWeddingUnderBudget(Wedding wedding){
-		logger.info("WeddingService.isWeddingUnderBudget was called for wedding: " + wedding);
+		//logger.info("WeddingService.isWeddingUnderBudget was called for wedding: " + wedding);
 		if (wedding.getVenue().getCost() + wedding.getCaterer().getCost() +
 		wedding.getFlorist().getCost() + wedding.getPhotographer().getCost() +
 		wedding.getMusician().getCost() > wedding.getBudget()){
@@ -65,7 +65,7 @@ public class WeddingServices {
 	}
 
 	public boolean isWeddingVenueAvailable (Wedding wedding){
-		logger.info("WeddingService.isWeddingVenueAvailable was called for wedding: " + wedding);
+		//logger.info("WeddingService.isWeddingVenueAvailable was called for wedding: " + wedding);
 		for (Wedding weddingCheck : getAllWeddings()){
 			if (weddingCheck.getDate().equals(wedding.getDate())){
 				if (weddingCheck.getVenue().equals(wedding.getVenue())){
@@ -77,7 +77,7 @@ public class WeddingServices {
 	}
 
 	public boolean isWeddingCatererAvailable (Wedding wedding){
-		logger.info("WeddingService.isWeddingCatererAvailable was called for wedding: " + wedding);
+		//logger.info("WeddingService.isWeddingCatererAvailable was called for wedding: " + wedding);
 		for (Wedding weddingCheck : getAllWeddings()){
 			if (weddingCheck.getDate().equals(wedding.getDate())){
 				if (weddingCheck.getCaterer().equals(wedding.getCaterer())){
@@ -89,7 +89,7 @@ public class WeddingServices {
 	}
 
 	public boolean isWeddingFloristAvailable (Wedding wedding){
-		logger.info("WeddingService.isWeddingFloristAvailable was called for wedding: " + wedding);
+		//logger.info("WeddingService.isWeddingFloristAvailable was called for wedding: " + wedding);
 		for (Wedding weddingCheck : getAllWeddings()){
 			if (weddingCheck.getDate().equals(wedding.getDate())){
 				if (weddingCheck.getFlorist().equals(wedding.getFlorist())){
@@ -101,7 +101,7 @@ public class WeddingServices {
 	}
 
 	public boolean isWeddingPhotographerAvailable (Wedding wedding){
-		logger.info("WeddingService.isWeddingPhotographerAvailable was called for wedding: " + wedding);
+		//logger.info("WeddingService.isWeddingPhotographerAvailable was called for wedding: " + wedding);
 		for (Wedding weddingCheck : getAllWeddings()){
 			if (weddingCheck.getDate().equals(wedding.getDate())){
 				if (weddingCheck.getPhotographer().equals(wedding.getPhotographer())){
@@ -113,7 +113,7 @@ public class WeddingServices {
 	}
 
 	public boolean isWeddingMusicianAvailable (Wedding wedding){
-		logger.info("WeddingService.isWeddingMusicianAvailable was called for wedding: " + wedding);
+		//logger.info("WeddingService.isWeddingMusicianAvailable was called for wedding: " + wedding);
 		for (Wedding weddingCheck : getAllWeddings()){
 			if (weddingCheck.getDate().equals(wedding.getDate())){
 				if (weddingCheck.getMusician().equals(wedding.getMusician())){
@@ -125,7 +125,7 @@ public class WeddingServices {
 	}
 
 	public boolean isWeddingValid (Wedding wedding){
-		logger.info("WeddingService.isWeddingValid was called for wedding: " + wedding);
+		//logger.info("WeddingService.isWeddingValid was called for wedding: " + wedding);
 		if (!(wedding.getDate().length() == 10)){
 			return false;
 		} else if (wedding.getBudget() <= 0){
@@ -136,7 +136,7 @@ public class WeddingServices {
 	}
 
 	public boolean isWeddingVenueAVenue (Wedding wedding){
-		logger.info("WeddingService.isWeddingVenueAVenue was called for wedding: " + wedding);
+		//logger.info("WeddingService.isWeddingVenueAVenue was called for wedding: " + wedding);
 		if(wedding.getVenue().getId() == 1){
 			return true;
 		} else{
@@ -145,7 +145,7 @@ public class WeddingServices {
 	}
 
 	public boolean isWeddingCatererACaterer (Wedding wedding){
-		logger.info("WeddingService.isWeddingVenueAVenue was called for wedding: " + wedding);
+		//logger.info("WeddingService.isWeddingVenueAVenue was called for wedding: " + wedding);
 		if(wedding.getCaterer().getId() == 2){
 			return true;
 		} else{
@@ -154,7 +154,7 @@ public class WeddingServices {
 	}
 
 	public boolean isWeddingFloristAFlorist (Wedding wedding){
-		logger.info("WeddingService.isWeddingVenueAVenue was called for wedding: " + wedding);
+		//logger.info("WeddingService.isWeddingVenueAVenue was called for wedding: " + wedding);
 		if(wedding.getFlorist().getId() == 3){
 			return true;
 		} else{
@@ -163,7 +163,7 @@ public class WeddingServices {
 	}
 
 	public boolean isWeddingPhotographerAPhotographer (Wedding wedding){
-		logger.info("WeddingService.isWeddingVenueAVenue was called for wedding: " + wedding);
+		//logger.info("WeddingService.isWeddingVenueAVenue was called for wedding: " + wedding);
 		if(wedding.getPhotographer().getId() == 4){
 			return true;
 		} else{
@@ -172,7 +172,7 @@ public class WeddingServices {
 	}
 
 	public boolean isWeddingMucicianAMucician (Wedding wedding){
-		logger.info("WeddingService.isWeddingVenueAVenue was called for wedding: " + wedding);
+		//logger.info("WeddingService.isWeddingVenueAVenue was called for wedding: " + wedding);
 		if(wedding.getMusician().getId() == 5){
 			return true;
 		} else{
