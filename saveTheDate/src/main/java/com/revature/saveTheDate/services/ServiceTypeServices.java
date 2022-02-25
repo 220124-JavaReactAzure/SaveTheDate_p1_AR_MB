3adc87d95994ad792d90f9e2bc5d75c8d944b89a
Michael Bronzo <82384863+BronzoMichael@users.noreply.github.com>
2022-02-24 20:13:22 -0500

    Update ServiceTypeServices.java

--- saveTheDate/src/main/java/com/revature/saveTheDate/services/ServiceTypeServices.java
+++ saveTheDate/src/main/java/com/revature/saveTheDate/services/ServiceTypeServices.java
@@ -9,29 +9,34 @@ import com.revature.saveTheDate.models.ServiceType;
 public class ServiceTypeServices {
 
 private final ServiceTypeDAO serviceTypeDAO;
+	private final Logger logger = LogManager.getLogger();
 	
 	public ServiceTypeServices(ServiceTypeDAO serviceTypeDAO) {
 		this.serviceTypeDAO = serviceTypeDAO;
+		logger.info("ServiceTypeService created");
 	}
 	
 	public boolean addServiceType(ServiceType serviceType) {
-		
-		return serviceTypeDAO.addServiceType(serviceType);
+		logger.info("ServiceTypeServices.addServiceType was called for ServiceType: " + serviceType);
+		if(isServiceTypeValid(serviceType)){
+			return serviceTypeDAO.addServiceType(serviceType);
+		} else{
+			return false;
+		}
 	}
 	
 	public List<ServiceType> getAllServiceTypes(){
+		logger.info("ServiceTypeServices.getAllServiceTypes was called");
 		return serviceTypeDAO.getAllServiceTypes();
-		
 	}
 	
 	public ServiceType getServiceTypeById(int id){
-		
+		logger.info("ServiceTypeServices.getServiceTypeById was called for id: " + id);
 		return serviceTypeDAO.getServiceTypeById(id);
 	}
 	
-	
 	public void updateServiceTypeWithSessionMethod(ServiceType serviceType) {
-		
+		logger.info("ServiceTypeServices.updateServiceTypeWithSessionMethod was called for ServiceType: " + serviceType);
 		serviceTypeDAO.updateServiceTypeWithSessionMethod(serviceType);
 	}
 	
@@ -41,6 +46,15 @@ private final ServiceTypeDAO serviceTypeDAO;
 	}*/
 	
 	public void deleteServiceTypeById(int id) {
+		logger.info("ServiceTypeServices.deleteServiceTypeById was called for id: " + id);
 		serviceTypeDAO.deleteServiceTypeById(id);
+	}
+
+	public boolean isServiceTypeValid(ServiceType serviceType){
+		if (serviceType.getId() <= 0 || serviceType.getId() >= 6 || serviceType.getService() == null){
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
